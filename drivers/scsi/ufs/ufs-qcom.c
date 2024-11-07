6d472cb702bf00d35418ed10dbb6a6f25b4c323d
mrsrimar22 <mrsrimar22@gmail.com>
2024-11-07 16:37:02 +0700

    scsi: ufs: Squash revert product changes
    At high utilization, clock scaling has an important role in
    partial file reading, instead of improving stability and efficiency,
    disabling clock scaling creates new problems on I/O when doing memory stress.
    
    Also revert HIGHPRIO and single release due to race conditions
    with vfs_path_lookup and also issues with e2fsck
    
    Squash revert of:
      - 5bfed396fa3e ("scsi: ufs: Use WQ_HIGHPRI for gating work.")
      - be79be2fef85 ("scsi: ufs: disable clock scaling")
      - f8f45f660ece ("scsi: ufs: atomic update for clkgating_enable")
      - 29fb462c5d0f ("scsi: ufs: call single_release to avoid memory leak")
      - 1d8adc37a82e ("scsi: ufs: Disable interrupt in reset path")
    
    Change-Id: Ib7606ea5530ff9b025901a42379affae43387029
    Signed-off-by: mrsrimar22 <mrsrimar22@gmail.com>
    Signed-off-by: skye-tachyon <sonicgames772@gmail.com>

--- drivers/scsi/ufs/ufs-qcom.c
+++ drivers/scsi/ufs/ufs-qcom.c
@@ -918,7 +918,6 @@ static int ufs_qcom_full_reset(struct ufs_hba *hba)
 {
 	struct ufs_qcom_host *host = ufshcd_get_variant(hba);
 	int ret = -ENOTSUPP;
-	bool reenable_intr = false;
 
 	host->hw_reset_count++;
 	host->last_hw_reset = (unsigned long)ktime_to_us(ktime_get());
@@ -933,10 +932,6 @@ static int ufs_qcom_full_reset(struct ufs_hba *hba)
 				ret);
 		goto out;
 	}
-
-	reenable_intr = hba->is_irq_enabled;
-	disable_irq(hba->irq);
-	hba->is_irq_enabled = false;
 
 	ret = reset_control_assert(hba->core_reset);
 	if (ret) {
@@ -956,11 +951,6 @@ static int ufs_qcom_full_reset(struct ufs_hba *hba)
 	if (ret)
 		dev_err(hba->dev, "%s: core_reset deassert failed, err = %d\n",
 				__func__, ret);
-
-	if (reenable_intr) {
-		enable_irq(hba->irq);
-		hba->is_irq_enabled = true;
-	}
 
 out:
 	return ret;
@@ -1524,6 +1514,7 @@ static void ufs_qcom_set_caps(struct ufs_hba *hba)
 	if (!host->disable_lpm) {
 		hba->caps |= UFSHCD_CAP_CLK_GATING;
 		hba->caps |= UFSHCD_CAP_HIBERN8_WITH_CLK_GATING;
+		hba->caps |= UFSHCD_CAP_CLK_SCALING;
 	}
 	hba->caps |= UFSHCD_CAP_AUTO_BKOPS_SUSPEND;
 
